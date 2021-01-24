426709c3242662b1185bb5bf6327fb09e499e03c
小峰 <160499844@qq.com>
2021-01-24 16:38:42 +0800

    修改整理文件夹名称

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ def mkdir(path):
         # 创建目录操作函数
         os.makedirs(path)
 
-        print("[+]" + path + ' 创建成功')
+        print("[+]" + path + ' 文件夹创建成功')
         return True
     else:
         # 如果目录存在则不创建，并提示目录已存在
-        print("[+]" + path + ' 已存在')
+        #print("[+]" + path + ' 文件夹已存在')
         return False
 
 
